c78e21144ae99d78b560b52b89b1687a2d0b05bb
bearInTheRoad <60107263+bearInTheRoad@users.noreply.github.com>
2020-07-12 17:31:44 +0800

    一点笔记 Page378

--- albumy/static/js/script.js
+++ albumy/static/js/script.js
@@ -2,34 +2,35 @@ $(function () {
     var default_error_message = 'Server error, please try again later.';
 
     $.ajaxSetup({
-        beforeSend: function (xhr, settings) {
-            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader('X-CSRFToken', csrf_token);
+        beforeSend: function (xhr, settings) { //xhr是XMLHttpRequest的意思
+            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) { //发出去的方法不是get head option trace而且没有跨站点
+                xhr.setRequestHeader('X-CSRFToken', csrf_token); //在request的header里加一个X-CSRFToken字段，用来验证CSRF
             }
         }
     });
 
-    $(document).ajaxError(function (event, request, settings) {
+    $(document).ajaxError(function (event, request, settings) { //三个参数，第一个是jQuery XMLHttpRequest对象，
+        //服务端的错误消息可以从这里获取；第二个是纯文本格式的错误状态，比如error，timeout等等，第三个是与HTTP错误相对应的原因短语比如Not Found
         var message = null;
-        if (request.responseJSON && request.responseJSON.hasOwnProperty('message')) {
+        if (request.responseJSON && request.responseJSON.hasOwnProperty('message')) { //如果request的响应里面本来就是Json，而且Json里面由property这个值，就拿来用
             message = request.responseJSON.message;
-        } else if (request.responseText) {
+        } else if (request.responseText) { //响应里没有json就看看有没有Text类型
             var IS_JSON = true;
             try {
-                var data = JSON.parse(request.responseText);
+                var data = JSON.parse(request.responseText); //看看这个text能不能解析成json
             }
             catch (err) {
                 IS_JSON = false;
             }
             if (IS_JSON && data !== undefined && data.hasOwnProperty('message')) {
-                message = JSON.parse(request.responseText).message;
+                message = JSON.parse(request.responseText).message; 
             } else {
                 message = default_error_message;
             }
         } else {
             message = default_error_message;
         }
-        toast(message, 'error');
+        toast(message, 'error'); //toast的第二个函数用来定义category类型。
     });
 
     var flash = null;
@@ -92,12 +93,12 @@ $(function () {
     }
 
     function update_followers_count(id) {
-        var $el = $('#followers-count-' + id);
+        var $el = $('#followers-count-' + id); //拼一个id出来，获取这个id所指向的元素
         $.ajax({
             type: 'GET',
             url: $el.data('href'),
             success: function (data) {
-                $el.text(data.count);
+                $el.text(data.count); //更新数字
             }
         });
     }
@@ -134,13 +135,13 @@ $(function () {
         var id = $el.data('id');
 
         $.ajax({
-            type: 'POST',
-            url: $el.data('href'),
-            success: function (data) {
-                $el.prev().show();
-                $el.hide();
-                update_followers_count(id);
-                toast(data.message);
+            type: 'POST', //定义http方法
+            url: $el.data('href'), //传入http的目标地址
+            success: function (data) { //success回调函数
+                $el.prev().show(); //因为关注和取消关注两个按钮市相邻的，取消关注在前，关注灾后，所以$el.prev()获取取消关注按钮，并用show展示出来
+                $el.hide(); //将关注按钮隐藏掉
+                update_followers_count(id); //调用update函数
+                toast(data.message); //显示提示消息。message内容由服务端定义，js这百年用toast把内容接住
             }
         });
     }
@@ -193,8 +194,8 @@ $(function () {
     }
 
     $('.profile-popover').hover(show_profile_popover.bind(this), hide_profile_popover.bind(this)); //this变量用于传入上下文
-    $(document).on('click', '.follow-btn', follow.bind(this));
-    $(document).on('click', '.unfollow-btn', unfollow.bind(this));
+    $(document).on('click', '.follow-btn', follow.bind(this)); //第一个参数是事件，第二个参数是选择器（即在哪个东西上触发事件），第三个参数是一旦触发传入的回调函数
+    $(document).on('click', '.unfollow-btn', unfollow.bind(this)); //我们不能直接$(.follow-btn).on(...)因为jQuery中快捷方法只能绑定到已经存在的元素上，而follow-btn是需要触发才会插入的东西
     $(document).on('click', '.collect-btn', collect.bind(this));
     $(document).on('click', '.uncollect-btn', uncollect.bind(this));
 
